01f16053f77ce288127b2712da878d5c9ed8ef26
Thehackernonymous <thehacknonymous@gmail.com>
2024-01-24 11:17:56 +0530

    fix index.js file

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,10 +20,10 @@ const Header = ({ setSearchQuery, openMediaModal, states }) => {
        <nav className="nav">
   <ul className="nav-list">
     <li className="nav-item">
-      <a href="/education-portal">Home</a>
+      <a href="/">Home</a>
     </li>
     <li className="nav-item media-btn">
-      <a href="/education-portal/Media">Media Section</a>
+      <a href="/Media">Media Section</a>
       
     </li>
   </ul>
